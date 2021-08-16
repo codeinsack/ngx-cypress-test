a576d19562edc858f029d054e055798b283fcaa7
codeinsack <codeinsack@gmail.com>
2021-08-16 17:06:47 +0300

    add different types of locators

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,8 +1,33 @@
+/// <reference types="cypress" />
 
 describe('Our first suite', () => {
 
   it('first test', () => {
 
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Form Layouts').click();
+
+    cy.get('[data-cy="imputEmail1"]');
+
   });
 
+
+  it.only('second test', () => {
+
+    cy.visit('/');
+    cy.contains('Forms').click();
+    cy.contains('Form Layouts').click();
+
+    cy.get('[data-cy="signInButton"]');
+
+    cy.contains('Sign in');
+    cy.get('#inputPassword3')
+      .parents('form')
+      .find('button')
+      .should('contain', 'Sign in')
+      .parents('form')
+      .find('nb-checkbox')
+      .click();
+  });
 });

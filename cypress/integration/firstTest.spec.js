d130c351dea266664ce8de3363022c0e01d04b06
codeinsack <codeinsack@gmail.com>
2021-08-16 21:39:32 +0300

    use web tables

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -136,7 +136,7 @@ describe('Our first suite', () => {
     cy.get('[type="checkbox"]').eq(1).check({ force: true });
   });
 
-  it.only('lists and dropdowns', () => {
+  it('lists and dropdowns', () => {
 
     cy.visit('/');
 
@@ -167,5 +167,47 @@ describe('Our first suite', () => {
         }
       });
     });
+  });
+
+  it.only('web tables', () => {
+
+    cy.visit('/');
+    cy.contains('Tables & Data').click();
+    cy.contains('Smart Table').click();
+
+    // 1
+    cy.get('tbody').contains('tr', 'Larry').then(tableRow => {
+      cy.wrap(tableRow).find('.nb-edit').click();
+      cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25');
+      cy.wrap(tableRow).find('.nb-checkmark').click();
+      cy.wrap(tableRow).find('td').eq(6).should('contain', '25');
+    });
+
+    // 2
+    cy.get('thead').find('.nb-plus').click();
+    cy.get('thead').find('tr').eq(2).then(tableRow => {
+      cy.wrap(tableRow).find('[placeholder="First Name"]').type('Eugene');
+      cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Furmanov');
+      cy.wrap(tableRow).find('.nb-checkmark').click();
+    });
+    cy.get('tbody tr').first().find('td').then(tableColumn => {
+      cy.wrap(tableColumn).eq(2).should('contain', 'Eugene');
+      cy.wrap(tableColumn).eq(3).should('contain', 'Furmanov');
+    });
+
+    // 3
+    const ages = [20, 30, 40, 200];
+
+    cy.wrap(ages).each(age => {
+      cy.get('thead [placeholder="Age"]').clear().type(age);
+      cy.wait(500);
+      cy.get('tbody tr').each(tableRow => {
+        if (age === 200) {
+          cy.wrap(tableRow).should('contain', 'No data found');
+        } else {
+          cy.wrap(tableRow).find('td').eq(6).should('contain', age);
+        }
+      });
+    })
   });
 });
